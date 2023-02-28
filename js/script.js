86dca9c168502eedae1fa59df645b5e262526c0d
sollendy <edlannino@tutanota.com>
2023-02-28 15:39:01 +0100

    preparato pseudocodice

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// come voglio strutturare il carosello?
+
+/*
+- preparare l'array con le immagini (sperando di capire come prendere quelle presenti nella consegna)
+- tradurre in variabili gli elem html
+- creare una variabile di indice
+--> premuta freccia su
+    ° l'indice aumenta di un'unità
+    ° mostrare l'immagine del array associata a quel valore di indice
+--> premuta freccia giù
+    ° l'indice diminuisce di una unità
+    ° mostrare l'immagine del array associata a quel valore di indice
+*/
