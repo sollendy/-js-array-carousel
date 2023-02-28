7a7e0ffcd6f8e86d59fc5c784403ff3d9cadf46d
sollendy <edlannino@tutanota.com>
2023-02-28 16:44:05 +0100

    esercizio in svolgimento

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@
     ° l'indice diminuisce di una unità
     ° mostrare l'immagine del array associata a quel valore di indice
 */
+
+// - preparare l'array con le immagini (sperando di capire come prendere quelle presenti nella consegna)
+
+const immagini = [
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp",
+];
+
+// tradurre in variabili gli elem html
+let frecciaSu = document.getElementById("freccia-su");
+let frecciaGiu = document.getElementById('freccia-giù');
+let 
